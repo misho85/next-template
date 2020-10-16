6a5847c17cc3afc879cd0effcd89cb7e27b5ffc3
M150 <stevovic@gmail.com>
2020-10-16 16:00:16 +0200

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+  <a href="https://krafter.dev">
+    <img
+      src="https://avatars0.githubusercontent.com/u/60414700?s=400&u=9c0d576ff33714dcdf11b159f0f3d775b2bac5ea&v=4"
+      height="80"
+      alt="krafter"
+      title="krafter"
+    />
+  </a>
+
+# Next.js - Template
+
+- Next.js
+- Styled Components
+- ESLint
+- Prettier
+- Stylelint
+- Husky
---
+++ pages/_app.jsx
@@ -0,0 +1,29 @@
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import * as gtag from '~lib/gtag';
+import { GlobalStyle, theme } from '~styles/.';
+
+const App = ({ Component, pageProps }) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = url => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+};
+
+export default App;

7495eb18bd14c9b1e73dac8e830e4624411810b7
M150 <stevovic@gmail.com>
2020-10-28 04:24:51 +0100

    update

--- styles/global.js
+++ styles/global.js
@@ -1,6 +1,9 @@
 import { createGlobalStyle } from 'styled-components';
+import { normalize } from 'styled-normalize';
 
 export default createGlobalStyle`
+ ${normalize}
+ 
   * {
     margin: 0;
     padding: 0;
